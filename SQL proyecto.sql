3f72ad5f629eb494e3a472cef02bfeded06a546d
PaolaNSDP <nadalesdepuerta@gmail.com>
2025-01-17 14:11:09 +0100

    nueva entrada

--- SQL proyecto.sql
+++ SQL proyecto.sql
@@ -1,25 +1,17 @@
 CREATE SCHEMA music_stream;
-
 USE music_stream;
-
 CREATE TABLE Artistas (
-    id INTEGER PRIMARY KEY,
-    nombre_artistas VARCHAR(255),
+	nombre_artistas VARCHAR(255),
     biografia TEXT,
     popularidad INTEGER,
-    reproducciones INT, 
+    reproducciones INT,
     artistas_similares VARCHAR(255)
 );
-
 CREATE TABLE AlbumesyCanciones (
     nombre_cancion VARCHAR(255),
     nombre_artista VARCHAR(50),
     genre VARCHAR(10),
     type VARCHAR(10),
     year INTEGER,
-    id VARCHAR(30) PRIMARY KEY,
-    id_artista INTEGER,
-    FOREIGN KEY (id_artista) REFERENCES Artistas(id)
+    id VARCHAR(30) PRIMARY KEY
 );
-
-
