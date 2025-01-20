d9689b66644759f76976e51703f442a187caddf4
Lucía <lucia18nozal@hotmail.com>
2025-01-20 14:50:20 +0100

    Query modificada y código con errores por ver

--- SQL proyecto.sql
+++ SQL proyecto.sql
@@ -1,6 +1,9 @@
 CREATE SCHEMA music_stream;
+
 USE music_stream;
+
 CREATE TABLE Artistas (
+	id_artistas INT AUTO_INCREMENT PRIMARY KEY,
 	nombre_artistas VARCHAR(255),
     biografia TEXT,
     popularidad INTEGER,
@@ -8,10 +11,12 @@ CREATE TABLE Artistas (
     artistas_similares TEXT(1000)
 );
 CREATE TABLE AlbumesyCanciones (
+    id_albumesycanciones VARCHAR(30) PRIMARY KEY,
     nombre_cancion VARCHAR(255),
     nombre_artista VARCHAR(50),
     genre VARCHAR(10),
     type VARCHAR(10),
-    year INTEGER,
-    id VARCHAR(30) PRIMARY KEY
+    year INT,
+    id_artista INT,
+    FOREIGN KEY (id_artista) REFERENCES Artistas(id_artistas)
 );
